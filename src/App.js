24e0d04aa4f6b4172a205f3261036caa410c11d0
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-05-17 11:01:40 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PokemonList from "./components/PokemonList";
 
 const App = () => {
+    const [search, setSearch] = useState("");
 
-  const [search, setSearch] = useState("")
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    };
 
-  const handleChange = (e) => {
-    setSearch(e.target.value)
-  }
+    return (
+        <div>
+            <input onChange={handleChange} />
 
-  return (
-    <div>
-      <input onChange={handleChange} />
-
-      <PokemonList searchTerm={search} />
-    </div>
-  );
+            <PokemonList searchTerm={search} />
+        </div>
+    );
 };
 
 export default App;
--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,14 +1,36 @@
-import React  from "react";
+import React from "react";
 
 function PokemonList({ searchTerm = "" }) {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=100`;
 
+    const [list, setList] = React.useState([]);
 
-  return (
-    <ul>
-    <li>test</li>
-    <li>test2</li>
-    </ul>
-  );
+    React.useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setList(data.results);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [url]);
+
+    React.useEffect(() => {
+        
+    }, [list]);
+
+    return (
+        <ul>
+            {list
+                .filter((pokemon) => {
+                    return pokemon.name.includes(searchTerm);
+                })
+                .map((pokemon) => (
+                    <li key={pokemon.name}>{pokemon.name}</li>
+            ))}
+        </ul>
+    );
 }
 
 export default PokemonList;
